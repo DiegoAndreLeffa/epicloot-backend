3caf4f894b9e6868916bd9137e8adc75970b6ba0
DiegoAndreLeffa <diegodocanto@gmail.com>
2024-07-08 17:43:50 -0300

    feat: create route user

---
+++ src/application/controllers/UserController.ts
@@ -0,0 +1,63 @@
+// src/application/controllers/UserController.ts
+import { Request, Response, NextFunction } from "express";
+import { UserService } from "../../domain/services/UserService";
+import { AppError } from "../../interfaces/http/middleware/errors";
+
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  async register(req: Request, res: Response, next: NextFunction): Promise<void> {
+    const { name, email, password } = req.body;
+    try {
+      const user = await this.userService.registerUser(name, email, password);
+      res.status(201).json(user);
+    } catch (error: any) {
+      next(new AppError(error.message));
+    }
+  }
+
+  async getUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+    const id = req.params.id;
+    try {
+      const user = await this.userService.getUserById(id);
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        next(new AppError("User not found", 404));
+      }
+    } catch (error: any) {
+      next(error);
+    }
+  }
+
+  async updateUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+    const id = req.params.id;
+    const updateData = req.body;
+    try {
+      const user = await this.userService.updateUser(id, updateData);
+      res.status(200).json(user);
+    } catch (error: any) {
+      next(new AppError(error.message));
+    }
+  }
+
+  async deleteUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+    const id = req.params.id;
+    try {
+      await this.userService.deleteUser(id);
+      res.status(204).send();
+    } catch (error: any) {
+      next(new AppError(error.message));
+    }
+  }
+
+  async authenticateUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+    const { email, password } = req.body;
+    try {
+      const user = await this.userService.authenticateUser(email, password);
+      res.status(200).json(user);
+    } catch (error: any) {
+      next(new AppError(error.message, 401));
+    }
+  }
+}
--- src/interfaces/http/routes.ts
+++ src/interfaces/http/routes.ts
@@ -1,5 +1,20 @@
 import { Router } from "express";
 
+import { TypeORMUserRepository } from "../../infrastructure/repositories/TypeORMUserRepository";
+import { UserService } from "../../domain/services/UserService";
+import { UserController } from "../../application/controllers/UserController";
+
 const router = Router();
+
+const userRepository = new TypeORMUserRepository();
+const userService = new UserService(userRepository);
+const userController = new UserController(userService);
+
+router.post("/users", (req, res, next) => userController.register(req, res, next));
+router.get("/users/:id", (req, res, next) => userController.getUser(req, res, next));
+router.put("/users/:id", (req, res, next) => userController.updateUser(req, res, next));
+router.delete("/users/:id", (req, res, next) => userController.deleteUser(req, res, next));
+router.post("/users/authenticate", (req, res, next) => userController.authenticateUser(req, res, next));
+
 
 export default router;
